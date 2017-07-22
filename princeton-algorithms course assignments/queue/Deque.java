bdb856b537372c9f291970cfd6b4c3033e016f3e
Mahmoud Adas <mido3ds@gmail.com>
2017-07-22 15:30:10 +0200

    added alot to Deque

--- princeton-algorithms course assignments/queue/Deque.java
+++ princeton-algorithms course assignments/queue/Deque.java
@@ -1,28 +1,135 @@
+import java.util.Iterator;
+import java.util.*;
+
 public class Deque<Item> implements Iterable<Item> {
-    // construct an empty deque
-    public Deque() {}
+    private Item[] array;
+    private int r = 1, l = 1;
+    private int size = 0;
 
-    // is the deque empty?
-    public boolean isEmpty() {}
+    /** construct an empty deque */
+    public Deque() {
+        array = (Item[]) new Object[3];
+    }
 
-    // return the number of items on the deque
-    public int size() {}
+    /** is the deque empty? */
+    public boolean isEmpty() {
+        return size == 0;
+    }
 
-    // add the item to the front
-    public void addFirst(Item item) {}
+    /** return the number of items on the deque */
+    public int size() {
+        return size;
+    }
 
-    // add the item to the end
-    public void addLast(Item item) {}
+    /** add the item to the front */
+    public void addFirst(Item item) {
+        if (item == null) throw new java.lang.IllegalArgumentException();
 
-    // remove and return the item from the front
-    public Item removeFirst() {}
+        if (r == array.length - 1) {
+            increaseToRight();
+        }
 
-    // remove and return the item from the end
-    public Item removeLast() {}
+        array[++r] = item;
+        size++;
+    }
 
-    // return an iterator over items in order from front to end
-    public Iterator<Item> iterator() {}
+    private void increaseToRight() {
+        assert size != 0;
 
-    // unit testing (optional)
+        Item[] newArray = (Item[]) new Object[size + (int) (.5 * size)];
+
+        // copy
+        for (int i = l; i <= r; i++) {
+            newArray[i] = array[i];
+        }
+
+        array = newArray;
+    }
+
+    /** add the item to the end */
+    public void addLast(Item item) {
+        if (item == null) throw new java.lang.IllegalArgumentException();
+
+        if (l == 0) {
+            increaseToLeft();
+        }
+
+        array[--l] = item;
+        size++;
+    }
+
+    private void increaseToLeft() {
+        assert size != 0;
+
+        int addedSize = (int) (.5 * size);
+        Item[] newArray = (Item[]) new Object[size + addedSize];
+
+        /** copy */
+        for (int i = l; i <= r; i++) {
+            newArray[addedSize + i] = array[i];
+        }
+
+        array = newArray;
+    }
+
+    /** remove and return the item from the front */
+    public Item removeFirst() {
+        if (size == 0) throw new java.util.NoSuchElementException();
+
+        Item item = array[r];
+
+        r++;
+        size--;
+
+        int leftSpace = r;
+        if ((int) .5 * size < leftSpace) shrinkFromLeft();
+
+        return item;
+    }
+
+    /** left space gets .5 smaller */
+    private void shrinkFromLeft() {
+        assert size != 0;
+
+        
+    }
+
+    /** remove and return the item from the end */
+    public Item removeLast() {
+        if (size == 0) throw new java.util.NoSuchElementException();
+
+        size--;
+        return array[l--];
+    }
+    
+    /** right space gets .5 smaller */
+    private void shrinkFromRight() {}
+
+    /** return an iterator over items in order from front to end */
+    public Iterator<Item> iterator() {
+        return new DequeIterator<>(0, this); // TODO
+    }
+
+    private class DequeIterator<Item> implements Iterator<Item> {
+        private int index;
+        private Deque<Item> deq;
+
+        public DequeIterator(int startingIndex, Deque<Item> d) {
+            index = startingIndex;
+            deq = d;
+        }
+
+        public boolean hasNext() {
+            return false; // TODO
+        }
+
+        public Item next() {
+            return deq.array[++index]; // TODO
+        }
+
+        public void remove() {}
+    }
+
+    /** unit testing */
     public static void main(String[] args) {}
 }
--- princeton-algorithms course assignments/queue/RandomizedQueue.java
+++ princeton-algorithms course assignments/queue/RandomizedQueue.java
@@ -1,24 +1,50 @@
+import java.util.Iterator;
+
 public class RandomizedQueue<Item> implements Iterable<Item> {
     // construct an empty randomized queue
     public RandomizedQueue() {}
 
     // is the queue empty?
-    public boolean isEmpty() {}
+    public boolean isEmpty() {
+        return false; // TODO
+    }
 
     // return the number of items on the queue
-    public int size() {}
+    public int size() {
+        return 0; // TODO
+    }
 
     // add the item
-    public void enqueue(Item item) {}
+    public void enqueue(Item item) {
+        // TODO
+    }
 
     // remove and return a random item
-    public Item dequeue() {}
+    public Item dequeue() {
+        // TODO
+    }
 
     // return (but do not remove) {} a random item
-    public Item sample() {}
+    public Item sample() {
+        // TODO
+    }
 
     // return an independent iterator over items in random order
     public Iterator<Item> iterator() {}
+
+    private class RandomizedQueueIterator<Item> implements Iterator<Item> {
+        public RandomizedQueueIterator() {}
+
+        public boolean hasNext() {
+            return false; // TODO
+        }
+
+        public Item next() {
+            // TODO
+        }
+
+        public void remove() {}
+    }
 
     // unit testing (optional) {}
     public static void main(String[] args) {}
